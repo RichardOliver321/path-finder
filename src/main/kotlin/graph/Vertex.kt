9ec74182004c074266dea05945ce1c1855cda6d0
Richard Oliver <richard.oliver321@gmail.com>
2019-10-16 21:05:59 +0200

    Added sub classes to AStar

--- src/main/kotlin/graph/Vertex.kt
+++ src/main/kotlin/graph/Vertex.kt
@@ -4,6 +4,7 @@ class Vertex(val x: Int, val y: Int) {
     var isStartPoint: Boolean = false
     var isEndPoint: Boolean = false
     var visited: Boolean = false
+    var parent: Vertex? = null
     var neighbors:  MutableSet<Vertex> = HashSet()
     var edges: MutableSet<Edge> = HashSet()
     var weight: Int = 0//(Math.random() * 99 + 1).toInt()
--- src/main/kotlin/path/AStarSearch.kt
+++ src/main/kotlin/path/AStarSearch.kt
@@ -1,11 +1,11 @@
 package path
 
+import graph.Edge
 import graph.SearchableGraph
 import graph.Vertex
 import java.util.*
-import kotlin.collections.ArrayList
 
-class AStarSearch :  PathFindingAlgo  {
+abstract class AStarSearch : PathFindingAlgo {
 
     override fun findShortestPath(graph: SearchableGraph): SearchableGraph {
         aStarSearch(graph)
@@ -19,11 +19,50 @@ class AStarSearch :  PathFindingAlgo  {
 
         openList.add(graph.startingVertex)
 
+        graph.startingVertex.weight = 0;
 
+
+        while (openList.isNotEmpty()) {
+            val vertex = openList.first();
+            openList.remove(vertex);
+
+            vertex.edges.forEach { edge ->
+                setParent(edge, vertex)
+                calculateCost(edge)
+                estimateDistanceToGoal()
+            }
+        }
 
     }
 
-    fun calculateCost() {
+    abstract fun estimateDistanceToGoal()
 
+    private fun setParent(edge: Edge, parent: Vertex) {
+        edge.toVertex.parent = parent
     }
+
+    private fun calculateCost(edge: Edge) {
+        val vertex = edge.toVertex
+        vertex.weight += vertex.parent!!.weight + edge.distance
+    }
+}
+
+class ManhattenAStar : AStarSearch() {
+    override fun estimateDistanceToGoal() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+class Diagonal : AStarSearch() {
+    override fun estimateDistanceToGoal() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class Euclidian : AStarSearch() {
+    override fun estimateDistanceToGoal() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
